aa1122b1ded988708d9706d0019ca16163bb1ba4
BreElle Wells <breellejordyn@gmail.com>
2023-07-10 19:16:37 -0600

    review: control flow lab

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+  if (someValue < 400) {
+    return 'This one is on me!';
+  }
+  else if (someValue < 2001) {
+    return 'That will be twenty bucks.';
+  }
+  else if (someValue < 2501) {
+    return 'I will gladly take your thirty bucks.';
+  }
+  else {
+    return 'No can do.';
+  }
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someValue){
+  return someValue === 'NYC' ? 'Ok, sounds good.' : 'No go.'
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip) {
+    case 'generous':
+      return 'Thank you so much.';
+      break;
+    case 'not as generous':
+      return 'Thank you.';
+      break;
+    default:
+      return 'Bye.';
+  }
 }
+
+// describe('switchOnCharmFromTip()', function () {
+//   it('should return "Thank you so much." if the tip is generous', function () {
+//     expect(switchOnCharmFromTip('generous')).to.equal('Thank you so much.');
+//   });
+
+//   it('should return "Thank you." if the tip is not as generous', function () {
+//     expect(switchOnCharmFromTip('not as generous')).to.equal('Thank you.');
+//   });
+
+//   it('should return "Bye." if anything else', function () {
+//     expect(switchOnCharmFromTip('thanks for everything')).to.equal('Bye.');
+//   });
+// });
+// });
